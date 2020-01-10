8f0bbcc0f5841563656c98451e8cfc35ffea14b9
Chance Harmon <henrikillsit@gmail.com>
2020-01-10 09:17:56 -0800

    little bit of progress, not happy with UI

---
+++ src/layout/Calculator/Calculator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Screen from './Screen/Screen'
+
+const calculator = () => (
+  <main className="calculator">
+    <Screen />
+  </main>
+);
+
+export default calculator;
